82e6424cf2668639eb34212c9c1cda76444e4e86
Mdev122 <cimmerian422@gmail.com>
2026-06-21 14:05:29 +0200

    akordeon

--- dist/js/script.js
+++ dist/js/script.js
@@ -53,15 +53,60 @@
   };
 
   class Product {
-    // ZMIANA: Dodanie argumentów (id, data) do konstruktora
     constructor(id, data){
       const thisProduct = this;
 
-      // ZMIANA: Zapisanie argumentów jako właściwości instancji
       thisProduct.id = id;
       thisProduct.data = data;
 
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+
       console.log('new Product:', thisProduct);
+    }
+
+    renderInMenu(){
+      const thisProduct = this;
+
+      /* Krok 1: generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+
+      /* Krok 2: create element using utils.createDOMFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /* Krok 3: find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      /* Krok 4: add element to menu */
+      menuContainer.appendChild(thisProduct.element);
+    }
+
+    initAccordion(){
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      // Szukamy nagłówka (clickable) TYLKO wewnątrz elementu tego konkretnego produktu
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+        event.preventDefault();
+
+        /* find active product (product that has active class) */
+        // Szukamy na CAŁEJ stronie produktu, który aktualnie ma klasę active
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        // Jeśli znaleźliśmy aktywny produkt i NIE JEST on tym, który właśnie kliknęliśmy -> zamykamy go!
+        if (activeProduct && activeProduct !== thisProduct.element) {
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+
+        /* toggle active class on thisProduct.element */
+        // Przełączamy klasę active na klikniętym produkcie (doda jeśli nie ma, zabierze jeśli jest)
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
     }
   }
 
@@ -88,7 +133,7 @@
       console.log('settings:', settings);
       console.log('templates:', templates);
 
-      thisApp.initData();
+      thisApp.data = dataSource;
       thisApp.initMenu();
     },
   };
